b931785a04199fa758772fb7aeaa6815c2925701
Unswer <35911324+UnswerGIT@users.noreply.github.com>
2018-07-06 10:39:10 +0800

    First commit

---
+++ www/test.py
@@ -0,0 +1,12 @@
+import orm 
+from models import User,Blog,Comment
+import asyncio
+
+loop = asyncio.get_event_loop()
+
+async def test():
+	await orm.create_pool(loop=loop,host='127.0.0.1',port=3306,user='root',password='password',db='awesome')
+	u=User(name='Test',email='test@exmple.com',passwd='1234567890',image='about:blank',id='123')
+	await u.save()
+loop.run_until_complete(test())
+loop.close()
